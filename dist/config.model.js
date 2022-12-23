a1dbf0788b3d5178bbccda3c1fe8242f1946209d
Srikahn <49712202+Srikahn@users.noreply.github.com>
2022-12-23 20:48:14 +0100

    Cleaning dist from repository

--- dist/config.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_PORT = exports.DB_PASSWORD = exports.DB_USER = exports.DB_DATABASE = exports.DB_HOST = void 0;
-exports.DB_HOST = "";
-exports.DB_DATABASE = "";
-exports.DB_USER = "";
-exports.DB_PASSWORD = "";
-exports.DB_PORT = 3306;
